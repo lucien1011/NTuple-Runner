d28cef4484cb1a0b6f170ff28367568a6b92143e
lucien1011 <lucien1011@gmail.com>
2018-08-29 12:24:34 -0400

    Added haddAllSamples

--- EndSequence.py
+++ EndSequence.py
@@ -4,22 +4,29 @@ from Hadder import Hadder
 import os,ROOT
 
 class EndSequence(object):
-    def __init__(self,skipHadd=False):
+    def __init__(self,skipHadd=False,haddAllSamples=False):
         self.moduleList = []
         self.collector = Collector()
         self.hadder = Hadder()
         self.skipHadd = skipHadd
+        self.haddAllSamples = haddAllSamples
+        self.allSampleName = "AllSample"
 
     def run(self,inputInfo,componentList,mergeSampleDict={}):
         self.collector.makeSampleList(componentList)
         self.collector.makeMergedSampleList(componentList,mergeSampleDict)
         for sampleName in self.collector.mergeSamples:
             self.makedirs(inputInfo.outputDir+sampleName)
-        if not self.skipHadd:
+        if not self.skipHadd: 
             for sampleName in self.collector.samples:
                 print "Hadding "+sampleName
                 self.hadder.makeHaddScript(inputInfo.outputDir+sampleName,[sampleName],inputInfo)
                 self.hadder.haddSampleDir(inputInfo.outputDir+sampleName)
+            if self.haddAllSamples:
+                print "Hadding "+self.allSampleName
+                self.makedirs(inputInfo.outputDir+self.allSampleName)
+                self.hadder.makeHaddScript(inputInfo.outputDir+self.allSampleName,self.collector.samples,inputInfo)
+                self.hadder.haddSampleDir(inputInfo.outputDir+self.allSampleName)
             for sampleName in self.collector.mergeSamples:
                 print "Hadding (and merging) "+sampleName
                 self.hadder.makeHaddScript(inputInfo.outputDir+sampleName,self.collector.mergeSampleDict[sampleName],inputInfo)

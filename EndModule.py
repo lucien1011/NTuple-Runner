ad40a20b35a514dcd6baed4127c73fdc00402909
lucien1011 <lucien1011@gmail.com>
2018-10-21 12:07:36 -0400

    Added makedirs_with_file_path in EndModule

--- EndModule.py
+++ EndModule.py
@@ -8,3 +8,9 @@ class EndModule(object):
     def makedirs(outputDir):
         if not os.path.exists(os.path.abspath(outputDir)):
             os.makedirs(os.path.abspath(outputDir))
+
+    @staticmethod
+    def makedirs_with_file_path(outputPath):
+        dir_path = os.path.abspath(os.path.dirname(outputPath))
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)

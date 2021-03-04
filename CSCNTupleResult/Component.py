8158c8fcc593032366d1af2d22707952bd98c6cc
lucien1011 <lucien1011@gmail.com>
2021-03-04 11:33:33 -0500

    Removed decprecated modules

--- CSCNTupleResult/Component.py
+++
@@ -1,7 +0,0 @@
-class Component(object):
-    def __init__(self,name,path):
-        self.name = name
-        self.path = path
-
-    def __str__(self):
-        return self.name+" "+self.path
--- CSCNTupleResult/ComponentProcessor.py
+++
@@ -1,6 +0,0 @@
-class ComponentProcessor(object):
-    def __init__(self):
-        pass
-
-    def __call__(self,component):
-        print "Processing"
